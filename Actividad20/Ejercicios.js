4afbae522e73610a36d780997b737f86c66aff82
Ayman <oumgharayman2004@gmail.com>
2024-05-13 22:31:55 +0200

    creation Actividad20

---
+++ Actividad20/Ejercicios.js
@@ -0,0 +1,6 @@
+function promedio() {
+    let numero = parseInt(document.getElementById('numero').value);
+    const arrayNumeros = [];
+
+    document.getElementById('resultado').innerText = `La cantidad de números pares entre ${inicio} y ${fin} es: ${contador}`;
+}
